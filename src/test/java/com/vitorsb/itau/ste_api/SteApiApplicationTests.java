c27c94a024bf9957ee214a5c746cb3c9610b6c0d
sbx <vsbreturn@gmail.com>
2025-02-04 19:21:30 -0300

    Commit de inicializacao do Projeto ITAU

---
+++ src/test/java/com/vitorsb/itau/ste_api/SteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitorsb.itau.ste_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
